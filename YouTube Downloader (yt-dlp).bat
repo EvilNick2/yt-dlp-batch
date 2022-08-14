b0e7e4fc307cbc904d8805e89a0a35c294916767
TheRambotnic <lucasrafaellonguini09@gmail.com>
2022-08-14 00:37:54 -0300

    Updated batch

--- YouTube Downloader (yt-dlp).bat
+++ YouTube Downloader (yt-dlp).bat
@@ -1,6 +1,6 @@
 @echo off
 
-title youtube-dl - Download Your Shit (https://github.com/yt-dlp/yt-dlp)
+title YouTube Downloader (yt-dlp)
 
 :askMode
 	echo Modes:
@@ -10,50 +10,51 @@ title youtube-dl - Download Your Shit (https://github.com/yt-dlp/yt-dlp)
 
 if "%mode%" gtr "2" (
 	echo.
-	echo THAT AIN'T IT CHIEF!
+	echo Not a recognized mode!
 	echo.
 	goto :askMode
 )
 
 if "%mode%" lss "1" (
 	echo.
-	echo THAT AIN'T IT CHIEF!
+	echo Not a recognized mode!
 	echo.
 	goto :askMode
 )
 
-echo.
-echo Extensions:
-echo m4a, mp3, mp4
-set /P extension="Choose extension: "
-
-echo.
-start urls.txt
-set /P pause="Paste your URLs into 'urls.txt', save the file then hit ENTER."
+if not exist "backend\urls.txt" (
+	type nul > backend\urls.txt
+	start backend\urls.txt
+	set /P pause="Paste your URLs into 'urls.txt', save the file then hit ENTER."
+) else (
+	echo.
+	start backend\urls.txt
+	set /P pause="Paste your URLs into 'urls.txt', save the file then hit ENTER."
+)
 
 if "%mode%" equ "1" (
 	set params=^
-		-f "bv[height<=1440][vcodec^=avc]+ba[ext=%extension%]" ^
-		-a urls.txt ^
+		-f "bv[height<=1440][vcodec^=avc]+ba[ext=m4a]" ^
+		-a backend\urls.txt ^
 		--embed-chapters ^
 		--embed-metadata ^
 		--embed-thumbnail ^
 		--convert-thumbnail jpg ^
 		--embed-subs ^
 		--sub-langs en.* ^
-		-o "D:\HDD Files\Videos\youtube-dl\Videos\%%(uploader)s\%%(title)s.%%(ext)s"
+		-o "downloads\Videos\%%(uploader)s\%%(title)s.%%(ext)s"
 )
 
 if "%mode%" equ "2" (
 	set params=^
 		-ciw ^
-		-a urls.txt ^
-		-f %extension% ^
+		-a backend\urls.txt ^
+		-f m4a ^
 		--embed-metadata ^
-		-o "D:\HDD Files\Videos\youtube-dl\Music\%%(uploader)s\%%(playlist_index)s - %%(title)s.%%(ext)s"
+		-o "downloads\Music\%%(uploader)s\%%(title)s.%%(ext)s"
 )
 
-yt-dlp.exe ^
+backend\yt-dlp.exe ^
 %params%
 
 pause
