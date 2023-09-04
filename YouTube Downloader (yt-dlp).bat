36d9632350653f2b237852e09ff097b4ac34275f
TheRambotnic <lucasrafaellonguini09@gmail.com>
2023-09-04 18:36:45 -0300

    Optimized the code and added a new call to open the options again after downloading a file

--- YouTube Downloader (yt-dlp).bat
+++ YouTube Downloader (yt-dlp).bat
@@ -17,46 +17,27 @@ echo.
 	echo 1. Videos
 	echo 2. Music
 	echo.
-	set /P mode="Choose mode: "
 
-if "%mode%" gtr "2" (
-	echo.
-	echo Not a recognized mode!
-	echo.
-
-	goto :askMode
-)
+set /P "mode=Choose mode: "
 
 if "%mode%" lss "1" (
-	echo.
-	echo Not a recognized mode!
-	echo.
+    echo.
+    echo Not a recognized mode!
+    echo.
+    goto :askMode
+)
 
-	goto :askMode
+if "%mode%" gtr "2" (
+    echo.
+    echo Not a recognized mode!
+    echo.
+    goto :askMode
 )
 
 if "%mode%" equ "1" (
-	echo.
-	goto :videoOrdered
-)
-
-if "%mode%" equ "2" (
-	echo.
-	goto :musicOrdered
-)
-
-:videoOrdered
-	set /P videoPL="Make video playlist ordered? (Y/N) "
-
-if "%videoPL%" equ "" (
-	echo Input cannot be empty!
-	goto :videoOrdered
-)
-
-if "%videoPL%" equ "y" (
-	set params=^
-		--ffmpeg-location "backend\ffmpeg\ffmpeg.exe" ^
-		-f "bv[height<=1080][vcodec^=avc]+ba[ext=m4a]" ^
+    set params=^
+    	--ffmpeg-location "backend\ffmpeg\ffmpeg.exe" ^
+    	-f "bv[height<=1080][vcodec^=avc]+ba[ext=m4a]" ^
 		-a backend\video_urls.txt ^
 		--embed-chapters ^
 		--embed-metadata ^
@@ -66,35 +47,11 @@ if "%videoPL%" equ "y" (
 		--sub-langs en.* ^
 		-o "Downloads\Videos\%%(uploader)s\%%(playlist_index)s - %%(title)s.%%(ext)s"
 
-		goto :promptVideoUrl
+	set "prompt=promptVideoUrl"
 )
 
-if "%videoPL%" equ "n" (
-	set params=^
-		--ffmpeg-location "backend\ffmpeg\ffmpeg.exe" ^
-		-f "bv[height<=1080][vcodec^=avc]+ba[ext=m4a]" ^
-		-a backend\video_urls.txt ^
-		--embed-chapters ^
-		--embed-metadata ^
-		--embed-thumbnail ^
-		--convert-thumbnail jpg ^
-		--embed-subs ^
-		--sub-langs en.* ^
-		-o "Downloads\Videos\%%(uploader)s\%%(title)s.%%(ext)s"
-
-	goto :promptVideoUrl
-)
-
-:musicOrdered
-	set /P musicPL="Make audio playlist ordered? (Y/N) "
-
-if "%musicPL%" equ "" (
-	echo Input cannot be empty!
-	goto :musicOrdered
-)
-
-if "%musicPL%" equ "y" (
-	set params=^
+if "%mode%" equ "2" (
+    set params=^
 		--ffmpeg-location "backend\ffmpeg\ffmpeg.exe" ^
 		-ciw ^
 		-a backend\music_urls.txt ^
@@ -102,64 +59,45 @@ if "%musicPL%" equ "y" (
 		--embed-metadata ^
 		-o "Downloads\Music\%%(uploader)s\%%(playlist_index)s - %%(title)s.%%(ext)s"
 
-	goto :promptMusicUrl
+	set "prompt=promptMusicUrl"
 )
 
-if "%musicPL%" equ "n" (
-	set params=^
-		--ffmpeg-location "backend\ffmpeg\ffmpeg.exe" ^
-		-ciw ^
-		-a backend\music_urls.txt ^
-		-f m4a ^
-		--embed-metadata ^
-		-o "Downloads\Music\%%(uploader)s\%%(title)s.%%(ext)s"
-
-	goto :promptMusicUrl
-)
-
+:: Calls the function to prompt for either video or music URLs
+goto %prompt%
 
 :promptVideoUrl
-	if not exist "backend\video_urls.txt" (
-		type nul > backend\video_urls.txt
-		start backend\video_urls.txt
-
-		echo.
-		set /P pause="Paste your URLs into 'video_urls.txt', save the file then hit ENTER."
-
-		goto :download
-	) else (
-		start backend\video_urls.txt
-		
-		echo.
-		set /P pause="Paste your URLs into 'video_urls.txt', save the file then hit ENTER."
-
-		goto :download
-	)
-
+call :promptUrls "video_urls.txt" "video"
+goto :download
 
 :promptMusicUrl
-	if not exist "backend\music_urls.txt" (
-		type nul > backend\music_urls.txt
-		start backend\music_urls.txt
+call :promptUrls "music_urls.txt" "music"
+goto :download
 
-		echo.
-		set /P pause="Paste your URLs into 'music_urls.txt', save the file then hit ENTER."
+:promptUrls
+if not exist "backend\%~1" (
+    type nul > "backend\%~1"
+    start backend\%~1
 
-		goto :download
-	) else (
-		start backend\music_urls.txt
-		
-		echo.
-		set /P pause="Paste your URLs into 'music_urls.txt', save the file then hit ENTER."
+    echo.
+    set /P "pause=Paste your URLs into '%~1', save the file then hit ENTER."
+) else (
+    start backend\%~1
 
-		goto :download
-	)
+    echo.
+    set /P "pause=Paste your URLs into '%~1', save the file then hit ENTER."
+)
+exit /b
 
 :download
-	backend\yt-dlp.exe ^
-	%params%
+backend\yt-dlp.exe %params%
+echo.
+echo ============================
+echo      DOWNLOAD COMPLETE!     
+echo ============================
+echo.
 
-	echo.
-	echo DOWNLOAD COMPLETE!
+:: Open the options again
+goto :askMode
 
 pause
+exit /b
